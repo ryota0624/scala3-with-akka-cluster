6affde663eaae049be92e52e3a4edee50c8bcef4
ryota0624 <tennis.suta6@gmail.com>
2021-08-20 17:26:08 +0900

    :sparkles: add ActorCommand impl

---
+++ src/main/scala/helper/Command.scala
@@ -0,0 +1,23 @@
+package helper
+
+import scala.reflect.ClassTag
+
+trait Command:
+  type Reply
+
+trait ReplyBuilder[C <: Command, -E]:
+  def buildReply(entity: E, command: C): command.Reply
+
+trait CommandProceessor[C <: Command, Event, -E] extends ReplyBuilder[C, E]:
+  def process(entity: E, command: C): Either[Throwable, Seq[Event]]
+  def buildReply(entity: E, command: C): command.Reply
+
+trait StatelesCommandProceessor[C <: Command, Event, -E] extends ReplyBuilder[C, E]:
+  def process(command: C): Either[Throwable, Seq[Event]]
+  def buildReply(entity: E, command: C): command.Reply
+
+trait Entity[C <: Command, Event] {
+  def process[C2 <: C](c: C2)(using processor:  CommandProceessor[C2, Event, this.type]): Either[Throwable, Seq[Event]] = processor.process(this, c)
+  def buildReply[C2 <: C](c: C2)(using processor: ReplyBuilder[C2, this.type]): c.Reply = processor.buildReply(this, c)
+}
+
---
+++ src/main/scala/helper/akka/ActorCommand.scala
@@ -0,0 +1,48 @@
+package helper.akka
+
+import akka.actor.typed.ActorRef
+import akka.pattern.StatusReply
+import akka.persistence.typed.scaladsl.Effect
+import helper.{Command, CommandProceessor, Entity, StatelesCommandProceessor}
+
+case class ActorCommand[C <: Command](cmd: C)(val replyTo: ActorRef[StatusReply[cmd.Reply]]):
+  type Reply = cmd.Reply
+  def apply(): (C, ActorRef[StatusReply[cmd.Reply]]) = (cmd, replyTo)
+  def as[C2 <: C]: ActorCommand[C2] =
+    val c = cmd.asInstanceOf[C2]
+    ActorCommand[C2](c)(replyTo.asInstanceOf[ActorRef[StatusReply[c.Reply]]])
+  def apply[C2 <: C]: ActorCommand[C2] =
+    as[C2]
+
+object ActorCommand:
+  extension [C <: Command](cmd: C)
+    def sendActor(replyTo: ActorRef[StatusReply[cmd.Reply]]) = ActorCommand(cmd)(replyTo)
+
+
+case class ActorCommandHandler[C <: Command, Event, E <: Entity[C, Event]]():
+  def apply[C2 <: C](actorCommand: ActorCommand[C2], entity: E)(using commandProceessor: CommandProceessor[C2, Event, E]) = {
+    entity.process[C2](actorCommand.cmd)
+      .map(events =>
+        Effect.persist[Event, Option[E]](events)
+          .thenReply(actorCommand.replyTo)
+          (state => {
+            state match {
+              case Some(entity) => StatusReply.success(entity.buildReply(actorCommand.cmd))
+              case None => StatusReply.error(new IllegalStateException("state should not be None"))
+            }
+          })
+      )
+  }
+  def apply[C2 <: C](actorCommand: ActorCommand[C2])(using commandProceessor: StatelesCommandProceessor[C2, Event, E]) = {
+    commandProceessor.process(actorCommand.cmd)
+      .map(events =>
+        Effect.persist[Event, Option[E]](events)
+          .thenReply(actorCommand.replyTo)
+          (state => {
+            state match {
+              case Some(entity) => StatusReply.success(entity.buildReply(actorCommand.cmd))
+              case None => StatusReply.error(new IllegalStateException("state should not be None"))
+            }
+          })
+      )
+  }
